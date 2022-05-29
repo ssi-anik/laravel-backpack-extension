ccb0a84563c3d82941811e6cba9577bbd1f7e67c
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2022-05-29 21:15:06 +0600

    added missing methods in filter interface

--- src/Contracts/Filter.php
+++ src/Contracts/Filter.php
@@ -6,4 +6,9 @@ use Illuminate\Contracts\Support\Arrayable;
 
 interface Filter extends Arrayable
 {
+    public function getValues(): mixed;
+
+    public function getLogic(): ?callable;
+
+    public function getFallbackLogic(): ?callable;
 }
