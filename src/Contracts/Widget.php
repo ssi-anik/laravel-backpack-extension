a8c12bb3cfc1f9aab7230bd01f2eef46b37970be
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2022-05-29 21:15:32 +0600

    added missing methods in widget interface

--- src/Contracts/Widget.php
+++ src/Contracts/Widget.php
@@ -6,4 +6,9 @@ use Illuminate\Contracts\Support\Arrayable;
 
 interface Widget extends Arrayable
 {
+    public function isHidden(): bool;
+
+    public function isFirst(): bool;
+
+    public function isLast(): bool;
 }
