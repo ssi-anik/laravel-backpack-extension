1976967cd843df3d75ce05414132c7c427d04c46
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2022-06-01 08:23:12 +0600

    ajax filter testcases added

---
+++ tests/Unit/Filters/AjaxFilterTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Anik\LaravelBackpack\Extension\Test\Unit\Filters;
+
+use Anik\LaravelBackpack\Extension\Filters\AjaxFilter;
+use Anik\LaravelBackpack\Extension\Filters\Filter;
+use Anik\LaravelBackpack\Extension\Test\TestCase;
+
+class AjaxFilterTest extends TestCase
+{
+    const METHODS = 'methods';
+    const PARAM = 'param';
+    const EXPECTED = 'expected';
+    const NAME = 'test_filter';
+
+    protected function getDefaultAttributes(): array
+    {
+        return ['name' => self::NAME, 'type' => 'select2_ajax',];
+    }
+
+    private function getFilterInstance(...$params): Filter
+    {
+        return new AjaxFilter(...$params);
+    }
+
+    public function getFilterInstanceUsingCreate(...$params): Filter
+    {
+        return AjaxFilter::create(...$params);
+    }
+
+    public function filterInstantiationDataProvider(): array
+    {
+        return [
+            'only name' => [
+                self::PARAM => [self::NAME],
+                self::EXPECTED => ['toArray' => $this->mergeAttributesWithDefault()],
+            ],
+            'name and method' => [
+                self::PARAM => [self::NAME, null, null, 'POST'],
+                self::EXPECTED => ['toArray' => $this->mergeAttributesWithDefault(['method' => 'POST'])],
+            ],
+            'name and url as value' => [
+                self::PARAM => [self::NAME, null, 'http://127.0.0.1'],
+                self::EXPECTED => ['toArray' => $this->mergeAttributesWithDefault(), 'getValues' => 'http://127.0.0.1'],
+            ],
+            'everything' => [
+                self::PARAM => [self::NAME, 'test_label', 'http://127.0.0.1', 'GET'],
+                self::EXPECTED => [
+                    'toArray' => $this->mergeAttributesWithDefault(['label' => 'test_label', 'method' => 'GET']),
+                    'getValues' => 'http://127.0.0.1',
+                ],
+            ],
+        ];
+    }
+
+    public function filterInstanceSetAttributes(): array
+    {
+        return [
+            'can set type' => [
+                self::METHODS => ['setType' => ['test_type']],
+                self::EXPECTED => $this->mergeAttributesWithDefault(['type' => 'test_type']),
+            ],
+            'can set minimum input length' => [
+                self::METHODS => ['setMinimumInputLength' => [5]],
+                self::EXPECTED => $this->mergeAttributesWithDefault(['minimum_input_length' => 5]),
+            ],
+            'can set select key' => [
+                self::METHODS => ['setSelectKey' => ['sk']],
+                self::EXPECTED => $this->mergeAttributesWithDefault(['select_key' => 'sk']),
+            ],
+            'can set select attribute' => [
+                self::METHODS => ['setSelectAttribute' => ['attrib']],
+                self::EXPECTED => $this->mergeAttributesWithDefault(['select_attribute' => 'attrib']),
+            ],
+            'can set method' => [
+                self::METHODS => ['setMethod' => ['POST']],
+                self::EXPECTED => $this->mergeAttributesWithDefault(['method' => 'POST']),
+            ],
+            'can set quiet time' => [
+                self::METHODS => ['setQuietTime' => [10]],
+                self::EXPECTED => $this->mergeAttributesWithDefault(['quiet_time' => 10]),
+            ],
+            'can set all values at once' => [
+                self::METHODS => [
+                    'setType' => ['test_type'],
+                    'setMinimumInputLength' => [5],
+                    'setSelectKey' => ['sk'],
+                    'setSelectAttribute' => ['attrib'],
+                    'setMethod' => ['POST'],
+                    'setQuietTime' => [10],
+                ],
+                self::EXPECTED => $this->mergeAttributesWithDefault(
+                    [
+                        'type' => 'test_type',
+                        'minimum_input_length' => 5,
+                        'select_key' => 'sk',
+                        'select_attribute' => 'attrib',
+                        'method' => 'POST',
+                        'quiet_time' => 10,
+                    ]
+                ),
+            ],
+        ];
+    }
+
+    public function test_filter_instance_is_by_default_select2_ajax_type()
+    {
+        $filter = $this->getFilterInstance(self::NAME);
+        $this->assertEquals($this->mergeAttributesWithDefault(), $filter->toArray());
+    }
+
+    /** @dataProvider filterInstantiationDataProvider */
+    public function test_filter_instantiate_with_create_method_pushes_provided_param_to_attribute($param, $expected)
+    {
+        $filter = $this->getFilterInstanceUsingCreate(...$param);
+        foreach ($expected as $method => $result) {
+            $this->assertEquals($result, call_user_func([$filter, $method]));
+        }
+    }
+
+    /** @dataProvider filterInstanceSetAttributes */
+    public function test_filter_instance($methods, $expected)
+    {
+        $filter = $this->getFilterInstanceUsingCreate(self::NAME);
+        foreach ($methods as $method => $params) {
+            call_user_func_array([$filter, $method], $params);
+        }
+        $this->assertEquals(
+            $expected,
+            $filter->toArray()
+        );
+    }
+}
